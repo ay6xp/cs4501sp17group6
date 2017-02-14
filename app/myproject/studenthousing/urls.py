29df10bb9892ac4dbea4ea1f44ff088e1562fbcb
Kirsten White <kaw3ut@virginia.edu>
2017-02-14 18:46:35 -0500

    views return JSON objects now, still need more functionality

--- app/myproject/studenthousing/urls.py
+++ app/myproject/studenthousing/urls.py
@@ -4,9 +4,10 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	# User CRUD
-	url(r'^users/$', views.user_list, name='user_list'),
-	# Listing CRUD
+	# User CRU(D)
+	url(r'^users/$', views.users, name='users'),
+	url(r'^users/(?P<username>\w+)/$', views.user_detail, name='user_detail'),
+	# Listing CRU(D)
 	url(r'^listings/$', views.listings, name='listings'),
 	url(r'^listings/(?P<listing_id>[0-9]+)/$', views.listing_detail, name='listing_detail'),
 ]
--- app/myproject/studenthousing/views.py
+++ app/myproject/studenthousing/views.py
@@ -1,6 +1,9 @@
+import json
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.core.urlresolvers import reverse
+from .models import User, Listing
 
 # Create your views here.
 
@@ -8,10 +11,40 @@ def index(request):
 	return HttpResponse("Welcome to the studenthousing index.")
 
 def listings(request):
-	return HttpResponse("Here are all of the listings.")
+	listings = Listing.objects.all().values()
+	if listings.count() == 0:
+		response_data = {}
+		response_data['result'] = 'error'
+		response_data['message'] = 'No listings exist at this time.'
+		return JsonResponse(response_data)
+	return JsonResponse(list(listings), safe=False)
 
 def listing_detail(request, listing_id):
-	return HttpResponse("This is the listing with id %s." % listing_id)
+	curr_listing = Listing.objects.all().filter(id=listing_id)
+	if curr_listing.count() == 1:
+		return JsonResponse(list(curr_listing))
+	else:
+		response_data = {}
+		response_data['result'] = 'error'
+		response_data['message'] = 'No listing exists with id %s.' % listing_id
+		return JsonResponse(response_data)
 
-def user_list(request):
-	return HttpResponse("Here are all of the users.")
+def users(request):
+	users = User.objects.all().values('username', 'email')
+	if users.count() == 0:
+		response_data = {}
+		response_data['result'] = 'error'
+		response_data['message'] = 'No users exist at this time.'
+		return JsonResponse(response_data)
+	else:
+		return JsonResponse(list(users))
+
+def user_detail(request, username):
+	curr_user = User.objects.all().filter(username=username)
+	if curr_user.count() == 1:
+		return JsonResponse(list(curr_user))
+	else:
+		response_data = {}
+		response_data['result'] = 'error'
+		response_data['message'] = 'No user exists with username %s.' % username
+		return JsonResponse(response_data)
