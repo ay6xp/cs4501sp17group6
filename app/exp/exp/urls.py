0b0fb224a9008fde69f90e3ba9f29d62b739c272
Kirsten White <kaw3ut@virginia.edu>
2017-03-01 11:44:51 -0500

    recent_users and recent_listings

--- app/exp/exp/urls.py
+++ app/exp/exp/urls.py
@@ -4,9 +4,15 @@ from . import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+
+    # listings
     url(r'^api/v1/listings/$', views.get_all_listings, name='get_all_listings'),
     url(r'^api/v1/listings/(?P<id>[0-9]+)/$', views.get_listing, name='get_listing'),
-    url(r'^api/v1/listings/expiring_soon/$', views.get_expiring_soon_listings, name='get_expiring_soon_listings'), 
+    url(r'^api/v1/listings/expiring_soon/$', views.get_expiring_soon_listings, name='get_expiring_soon_listings'),
+    url(r'^api/v1/listings/recently_posted/$', views.get_recently_posted_listings, name='get_recently_posted_listings'), 
+
+    # users
     url(r'^api/v1/users/$', views.get_all_users, name='get_all_users'),
     url(r'^api/v1/users/(?P<id>[0-9]+)/$', views.get_user, name='get_user'),
+    url(r'^api/v1/users/recently_joined/$', views.get_recently_joined_users, name='get_recently_joined_users'),
 ]
--- app/exp/exp/views.py
+++ app/exp/exp/views.py
@@ -66,6 +66,39 @@ def get_expiring_soon_listings(request):
 	else:
 		return JsonResponse({'message': res['message']})
 
+def get_recently_posted_listings(request):
+	req = urllib.request.Request(_url('listings/'))
+	resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+	res = json.loads(resp_json)
+
+	if res['ok']:
+
+		recent_list = []
+
+		# check to see if each listing was posted within the past three days
+		for i in range(len(res['info'])):
+			# get string representation of post's date
+			posted_str = res['info'][i]['post_date']
+			# convert into date object
+			posted_date = datetime.strptime(posted_str, '%Y-%m-%d').date()
+			
+			# check dates
+			if posted_date >= datetime.now().date() - timedelta(days=3):
+				# this is one we should return in this view
+				recent_list.append(res['info'][i])
+			else:
+				# this is not one we should return in this view
+				pass
+
+		# is list empty?
+		if len(recent_list) == 0:
+			return JsonResponse({'message': 'no new listings were posted in the last 3 days'})
+
+		return JsonResponse({'info': recent_list})
+
+	else:
+		return JsonResponse({'message': res['message']})
+
 
 #
 #	Users
@@ -97,4 +130,34 @@ def get_active_users(request):
 	pass
 
 def get_recently_joined_users(request):
-	pass
+	req = urllib.request.Request(_url('users/'))
+	resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+	res = json.loads(resp_json)
+
+	if res['ok']:
+
+		recent_list = []
+
+		# check to see if each user joined within the past three days
+		for i in range(len(res['info'])):
+			# get string representation of post's date
+			joined_str = res['info'][i]['joined_date']
+			# convert into date object
+			joined_date = datetime.strptime(joined_str, '%Y-%m-%d').date()
+			
+			# check dates
+			if joined_date >= datetime.now().date() - timedelta(days=3):
+				# this is one we should return in this view
+				recent_list.append(res['info'][i])
+			else:
+				# this is not one we should return in this view
+				pass
+
+		# is list empty?
+		if len(recent_list) == 0:
+			return JsonResponse({'message': 'no new users have joined in the last 3 days'})
+
+		return JsonResponse({'info': recent_list})
+
+	else:
+		return JsonResponse({'message': res['message']})
