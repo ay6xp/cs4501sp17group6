e1e224eaefff7e12d87b70e8fbf79041244d5d09
Kirsten White <kaw3ut@virginia.edu>
2017-03-01 19:48:26 -0500

    trying to make web/views work

--- app/web/web/views.py
+++ app/web/web/views.py
@@ -7,22 +7,18 @@ import json
 from django.template import loader
 from django.http import HttpResponse
 
-# def index(request):
-#         all_listings = Listing.objects.all()
-#         context = { 'all_listings' : all_listings }
-#         return render(request, 'studenthousing/index.html', context)
-
-
 def _url(path):
     return 'http://exp-api:8000/api/v1/' + path
 
 
 def index(request):
 
-    req = requests.get('http://exp-api:8000/api/v1/listings/')
-    raise(Exception(req))
+    req = requests.get('http://exp-api:8000/api/v1/listings/').json()
+    all_listings = req['info']
+    print(req['info'])
+    #raise(Exception(req))
 
-    return render(request, 'home/index.html', {'all_listings': all_listings})
+    return render(request, 'index.html', {'all_listings': all_listings})
 
     # req = urllib.request.Request(_url('listings/'))
     # resp_json = urllib.request.urlopen(req).read().decode('utf-8')
