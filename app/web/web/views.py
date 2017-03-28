4d20dbf665424282ab482f3a4ca4bdd62c97a3b1
Laith Hasanian <Laith@d-172-25-99-185.dhcp.virginia.edu>
2017-03-27 20:29:31 -0400

    some progress on login page and form

--- app/web/web/views.py
+++ app/web/web/views.py
@@ -14,22 +14,27 @@ from django.core.urlresolvers import reverse
 
 def index(request):
     req = requests.get(settings.API_DIR + 'index/').json()
+
     return render(request, 'home/index.html', req)
+
 
 def listing(request):
     req = requests.get(settings.API_DIR + 'listings/').json()
     all_listings = req['info']
     return render(request, 'home/listing.html', {'all_listings': all_listings})
 
+
 def listing_exp_soon(request):
     req = requests.get(settings.API_DIR + 'listings/expiring_soon/').json()
     print(req)
     return JsonResponse(req)
 
+
 def listing_post_recently(request):
     req = requests.get(settings.API_DIR + 'listings/recently_posted/').json()
     print(req)
     return JsonResponse(req)
+
 
 def listing_detail(request, id):
     req = requests.get(settings.API_DIR + 'listings/' + str(id) + '/').json()
@@ -46,10 +51,12 @@ def user(request):
     all_users = req['info']
     return render(request, 'home/user.html', {'all_users': all_users})
 
+
 def user_recent(request):
     req = requests.get(settings.API_DIR + 'users/recently_joined/').json()
     print(req)
     return JsonResponse(req)
+
 
 def user_detail(request, id):
     req = requests.get(settings.API_DIR + 'users/' + str(id) + '/').json()
@@ -67,31 +74,32 @@ def login(request):
         return render(request, 'home/index.html', auth)
     if request.method == 'GET':
         login_form = LoginForm()
-        #next = request.GET.get('login') or reverse('index')
-        return render(request, 'home/login.html', {'form':login_form, 'auth':auth})
+        # next = request.GET.get('login') or reverse('index')
+        return render(request, 'home/login.html', {'form': login_form, 'auth': auth})
     f = LoginForm(request.POST)
     if not f.is_valid():
         login_form = LoginForm()
-        return render(request, 'home/login.html', {'errorMessage': "Please fill out all fields",'form': login_form})
+        return render(request, 'home/login.html', {'errorMessage': "Please fill out all fields", 'form': login_form})
     username = f.cleaned_data['username']
     password = f.cleaned_data['password']
-    response = requests.post(settings.API_DIR + 'login/', data={'username':username, 'password':password}).json()
-    if  response['ok'] == False:
-        #error occurred
+    response = requests.post(settings.API_DIR + 'login/', data={'username': username, 'password': password}).json()
+    if response['ok'] == False:
+        # error occurred
         login_form = LoginForm()
-        return render(request, 'home/login.html', {'errorMessage': response['resp'],'form': login_form})
+        return render(request, 'home/login.html', {'errorMessage': response['resp'], 'form': login_form})
     auth_token = response['resp']
     next = HttpResponseRedirect(reverse('index'))
     next.set_cookie('auth', auth_token)
     return next
 
+
 def register(request):
     auth = request.COOKIES.get('auth')
     # If we received a GET request instead of a POST request
-    if request.method =='GET':
+    if request.method == 'GET':
         # display the login form page
         register_form = RegisterForm()
-        return render(request, 'home/register.html', {'form': register_form, 'auth':auth})
+        return render(request, 'home/register.html', {'form': register_form, 'auth': auth})
 
     # Creates a new instance of our login_form and gives it our POST data
     f = RegisterForm(request.POST)
@@ -99,7 +107,8 @@ def register(request):
     # Check if the form instance is invalid
     if not f.is_valid():
         # Form was bad -- send them back to login page and show them an error
-        return render(request, 'home/register.html', {'msg': "Please fill out the registration form again.", 'form': RegisterForm, 'auth':auth})
+        return render(request, 'home/register.html',
+                      {'msg': "Please fill out the registration form again.", 'form': RegisterForm, 'auth': auth})
     else:
         # Sanitize fields
         username = f.cleaned_data['username']
@@ -107,8 +116,9 @@ def register(request):
         email = f.cleaned_data['email']
         phone_num = f.cleaned_data['phone_num']
 
-        response = requests.post(settings.API_DIR + 'users/register/', data={'username': username, 'password': password, 
-                                                                'email': email, 'phone_num': phone_num}).json()
+        response = requests.post(settings.API_DIR + 'users/register/', data={'username': username, 'password': password,
+                                                                             'email': email,
+                                                                             'phone_num': phone_num}).json()
 
         # Get next page
         next = reverse('login')
@@ -126,4 +136,3 @@ def register(request):
         response.set_cookie("auth", authenticator)
 
         return response
-
