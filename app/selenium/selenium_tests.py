28656ff8948b8fd0619d3f4df27f055c5b61a574
mhn94 <maggiehn94@yahoo.com>
2017-05-10 22:24:49 -0400

    added more selenium test cases

--- app/selenium/selenium_tests.py
+++ app/selenium/selenium_tests.py
@@ -11,45 +11,56 @@ class SeleniumTests(unittest.TestCase):
            desired_capabilities=DesiredCapabilities.CHROME
         )
 
-	def test_frontend(self):
+	def test_home_page(self):
 		driver = self.driver
 		driver.get("http://192.168.99.100:8000/home/")
 		assert "Welcome to Student Housing!" in driver.page_source
 
-	#test register
-	def test_register(self):
-		username = "test11"
-		password = "password"
-		email = "test1@gmail.com"
-		phone_num = "1234567890"
-
+	def test_view_all_listings(self):
 		driver = self.driver
-		driver.get("http://192.168.99.100:8000/register/")
-		driver.find_element_by_id("id_username").send_keys(username)
-		driver.find_element_by_id("id_password").send_keys(password)
-		driver.find_element_by_id("id_phone_num").send_keys(phone_num)
-		driver.find_element_by_css_selector("button.btn").click()
-		# driver.get("http://192.168.99.100:8000/home/")
-		# print(driver.page_source)
-		assert "successfully created" in driver.page_source
+		driver.get("http://192.168.99.100:8000/home/")
+		driver.find_element_by_xpath("/html/body/div/div[2]/div[1]/div[1]/p/a").click()
+		assert "Listings:" in driver.page_source
 
-	#test login
-	def test_login(self):
-		username = "test11"
-		password = "password"
-
+	def test_click_register(self):
 		driver = self.driver
-		driver.get("http://192.168.99.100:8000/login/")
-		driver.find_element_by_id("id_username").send_keys(username)
-		driver.find_element_by_id("id_password").send_keys(password)
-		driver.find_element_by_class_name("btn").click()
-		# print(driver.page_source)
-		# driver.find_element_by_link_text("Student Housing").click()
-		assert "You are logged in." in driver.page_source
+		driver.get("http://192.168.99.100:8000/home/")
+		driver.find_element_by_xpath("/html/body/nav/div/div[2]/p[2]/a").click()
+		assert "Register" in driver.page_source
 
-    # def test_create_listing(self):
+	def test_click_login(self):
+		driver = self.driver
+		driver.get("http://192.168.99.100:8000/home/")
+		driver.find_element_by_xpath("/html/body/nav/div/div[2]/p[3]/a").click()
+		assert "Login" in driver.page_source
 
-    # def test_search(self):
+	# def test_register(self):
+	# 	username = "test20"
+	# 	password = "password"
+	# 	email = "test@gmail.com"
+	# 	phone_num = "1234567890"
+
+	# 	driver = self.driver
+	# 	driver.get("http://192.168.99.100:8000/register/")
+	# 	driver.find_element_by_id("id_username").send_keys(username)
+	# 	driver.find_element_by_id("id_password").send_keys(password)
+	# 	driver.find_element_by_id("id_phone_num").send_keys(phone_num)
+	# 	driver.find_element_by_id("id_phone_num").submit()
+	# 	print(driver.page_source)
+	# 	register_status = driver.find_element_by_class_name("info")
+	# 	self.assertEqual("successfully created", register_status)
+
+	# def test_login(self):
+	# 	username = "test20"
+	# 	password = "password"
+
+	# 	driver = self.driver
+	# 	driver.get("http://192.168.99.100:8000/login/")
+	# 	driver.find_element_by_id("id_username").send_keys(username)
+	# 	driver.find_element_by_id("id_password").send_keys(password)
+	# 	driver.find_element_by_id("id_password").submit()
+	# 	login_status = driver.find_element_by_xpath("//*[@id='logged_in_status']/span").text
+	# 	self.assertEqual("You are logged in.", login_status)
 
 	def tearDown(self):
 		self.driver.close()
